650c51e5324188004e7a86fae5702230b601b3e8
SzilagyiBa <SzilagyiBa@301-10>
2026-03-26 11:18:41 +0100

    idő és simtimer

--- ZBSnake/ZBSnake/ZBSnake/MainWindow.xaml.cs
+++ ZBSnake/ZBSnake/ZBSnake/MainWindow.xaml.cs
@@ -6,8 +6,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using ZBSnake.Models;
 
 namespace ZBSnake
 {
@@ -16,9 +19,18 @@ namespace ZBSnake
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DispatcherTimer simTimer;
+        public SimulationTime Time = new SimulationTime();
         public MainWindow()
         {
+            simTimer = new DispatcherTimer();
+            simTimer.Interval = TimeSpan.FromSeconds(Time.TimeRate);
+            simTimer.Tick += SimTimer_Tick;
             InitializeComponent();
+        }
+        private void SimTimer_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
---
+++ ZBSnake/ZBSnake/ZBSnake/Models/SimulationTime.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBSnake.Models
+{
+    class SimulationTime
+    {
+        public DateTime CurrentTime { get; set; } = new DateTime(2024, 1, 1, 8, 0, 0);
+
+        public string CurrentDayProgression { get; set; } = "nappal";
+        public bool IsDay { get; set; } = true;
+
+        public TimeSpan TimeSpent { get; set; } = TimeSpan.Zero;
+        public double TimeRate { get; set; } = 1;
+
+        public DateTime MissionEndTime { get; set; } = new DateTime(2026, 1, 1, 0, 0, 0);
+
+
+        public void SetTime(int hour, int minute)
+        {
+            // Beállítjuk az órát és percet a jelenlegi napon belül
+            CurrentTime = new DateTime(CurrentTime.Year, CurrentTime.Month, CurrentTime.Day, hour, minute, 0);
+            UpdateDayState();
+        }
+
+        public void RemainingMissionTimeChange(MainWindow mw)
+        {
+            // Kiszámoljuk az eddig eltelt időt percben
+            double passedMinutes = TimeSpent.TotalMinutes;
+            double remainingMinutes = mw.maxMinutes - passedMinutes;
+            if (remainingMinutes < 0)
+                remainingMinutes = 0;
+            // Kiszámoljuk a hátralévő időt és beállítjuk a MissionEndTime-ot
+            TimeSpan remaining = TimeSpan.FromMinutes(remainingMinutes);
+            MissionEndTime = CurrentTime.Add(remaining);
+            // Frissítjük a megjelenített hátralévő időt
+            mw.hatralevoido.Text = $"{(int)remaining.TotalHours:00}:{remaining.Minutes:00}";
+        }
+
+        public void AddTime()
+        {
+            // 30 perc hozzáadása
+            TimeSpan step = TimeSpan.FromMinutes(1);
+            CurrentTime = CurrentTime.Add(step);
+            // Növeljük az összesen eltöltött időt is
+            TimeSpent = TimeSpent.Add(step);
+            // Frissítjük a napszakot
+            UpdateDayState();
+        }
+
+
+        public void UpdateDayState()
+        {
+            if (CurrentTime.Hour >= 6 && CurrentTime.Hour < 22)
+            {
+                IsDay = true;
+                CurrentDayProgression = "nappal";
+            }
+            else
+            {
+                IsDay = false;
+                CurrentDayProgression = "ejszaka";
+            }
+        }
+
+        public string GetCurrentTimeString()
+        {
+            return CurrentTime.ToString("HH:mm");
+        }
+    }
+}
