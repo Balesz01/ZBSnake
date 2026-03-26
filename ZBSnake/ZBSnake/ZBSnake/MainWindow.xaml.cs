9b81595df4f4325bd86ed710cb24a9013d62c983
SzilagyiBa <SzilagyiBa@301-10>
2026-03-26 11:35:59 +0100

    alap megjelentites

--- ZBSnake/ZBSnake/ZBSnake/MainWindow.xaml.cs
+++ ZBSnake/ZBSnake/ZBSnake/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using ZBSnake.Models;
 
@@ -17,51 +9,79 @@ namespace ZBSnake
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
     public partial class MainWindow : Window
     {
         public DispatcherTimer simTimer;
         public SimulationTime Time = new SimulationTime();
+
+        // Eltároljuk a kígyó aktuális haladási irányát (alapból menjen jobbra)
+        private Direction currentDirection = Direction.Right;
+
         public MainWindow()
         {
+            InitializeComponent();
+
             simTimer = new DispatcherTimer();
+            // A TimeRate határozza meg, milyen gyors a játék (pl. 0.1 vagy 0.2 másodperc)
             simTimer.Interval = TimeSpan.FromSeconds(Time.TimeRate);
             simTimer.Tick += SimTimer_Tick;
-            InitializeComponent();
+
+            // EL KELL INDÍTANI a timert, hogy a Tick események elkezdjenek lefutni!
+            simTimer.Start();
         }
+
         private void SimTimer_Tick(object sender, EventArgs e)
         {
-            
+
+            // MoveSnake(); 
+
+
+            // CheckCollisions();
+
+            // 3. Evés vizsgálat (Megevett egy almát?)
+            // Ha igen, nő a pontszám, hosszabbodik a kígyó, és esetleg gyorsul a timer:
+            // simTimer.Interval = TimeSpan.FromSeconds(UjGyorsabbIdo);
+
+            // 4. Képernyő (UI) frissítése / Kígyó újrarajzolása
+            // DrawGame();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            // A gombnyomás CSAK az irányt változtatja meg.
+            // Extra szabály: a kígyó nem fordulhat meg 180 fokban önmagába!
             switch (e.Key)
             {
                 case Key.W:
-
+                case Key.Up:
+                    if (currentDirection != Direction.Down)
+                        currentDirection = Direction.Up;
                     break;
+
                 case Key.S:
-
+                case Key.Down:
+                    if (currentDirection != Direction.Up)
+                        currentDirection = Direction.Down;
                     break;
+
                 case Key.A:
-
+                case Key.Left:
+                    if (currentDirection != Direction.Right)
+                        currentDirection = Direction.Left;
                     break;
+
                 case Key.D:
-
-                    break;
-                case Key.Space:
-
-                    break;
-                case Key.R:
-
-                    break;
-                case Key.T:
-
-                    break;
-                case Key.Z:
-
-                    break;
-                case Key.U:
+                case Key.Right:
+                    if (currentDirection != Direction.Left)
+                        currentDirection = Direction.Right;
                     break;
             }
         }
--- ZBSnake/ZBSnake/ZBSnake/Models/SimulationTime.cs
+++ ZBSnake/ZBSnake/ZBSnake/Models/SimulationTime.cs
@@ -6,69 +6,32 @@ using System.Threading.Tasks;
 
 namespace ZBSnake.Models
 {
-    class SimulationTime
+    public class SimulationTime
     {
-        public DateTime CurrentTime { get; set; } = new DateTime(2024, 1, 1, 8, 0, 0);
+        /// <summary>
+        /// A játék alapvető sebessége másodpercben (mennyi idő telik el két lépés között).
+        /// Kisebb szám = gyorsabb játék.
+        /// </summary>
+        public double TimeRate { get; set; }
 
-        public string CurrentDayProgression { get; set; } = "nappal";
-        public bool IsDay { get; set; } = true;
-
-        public TimeSpan TimeSpent { get; set; } = TimeSpan.Zero;
-        public double TimeRate { get; set; } = 1;
-
-        public DateTime MissionEndTime { get; set; } = new DateTime(2026, 1, 1, 0, 0, 0);
-
-
-        public void SetTime(int hour, int minute)
+        public SimulationTime()
         {
-            // Beállítjuk az órát és percet a jelenlegi napon belül
-            CurrentTime = new DateTime(CurrentTime.Year, CurrentTime.Month, CurrentTime.Day, hour, minute, 0);
-            UpdateDayState();
+            // Kezdeti sebesség beállítása (pl. 0.2 másodpercenként lép egyet a kígyó)
+            TimeRate = 0.2;
         }
 
-        public void RemainingMissionTimeChange(MainWindow mw)
+        /// <summary>
+        /// Ezt a metódust akkor hívhatod meg, ha a kígyó megevett egy almát, 
+        /// és szeretnéd, hogy a játék egy kicsit gyorsuljon.
+        /// </summary>
+        public void IncreaseSpeed()
         {
-            // Kiszámoljuk az eddig eltelt időt percben
-            double passedMinutes = TimeSpent.TotalMinutes;
-            double remainingMinutes = mw.maxMinutes - passedMinutes;
-            if (remainingMinutes < 0)
-                remainingMinutes = 0;
-            // Kiszámoljuk a hátralévő időt és beállítjuk a MissionEndTime-ot
-            TimeSpan remaining = TimeSpan.FromMinutes(remainingMinutes);
-            MissionEndTime = CurrentTime.Add(remaining);
-            // Frissítjük a megjelenített hátralévő időt
-            mw.hatralevoido.Text = $"{(int)remaining.TotalHours:00}:{remaining.Minutes:00}";
-        }
-
-        public void AddTime()
-        {
-            // 30 perc hozzáadása
-            TimeSpan step = TimeSpan.FromMinutes(1);
-            CurrentTime = CurrentTime.Add(step);
-            // Növeljük az összesen eltöltött időt is
-            TimeSpent = TimeSpent.Add(step);
-            // Frissítjük a napszakot
-            UpdateDayState();
-        }
-
-
-        public void UpdateDayState()
-        {
-            if (CurrentTime.Hour >= 6 && CurrentTime.Hour < 22)
+            // Ne engedjük, hogy a játék játszhatatlanul gyors legyen (pl. 0.05 alá ne menjen)
+            if (TimeRate > 0.05)
             {
-                IsDay = true;
-                CurrentDayProgression = "nappal";
+                // Minden evésnél csökkentjük az időközt 0.01 másodperccel
+                TimeRate -= 0.01;
             }
-            else
-            {
-                IsDay = false;
-                CurrentDayProgression = "ejszaka";
-            }
-        }
-
-        public string GetCurrentTimeString()
-        {
-            return CurrentTime.ToString("HH:mm");
         }
     }
 }
